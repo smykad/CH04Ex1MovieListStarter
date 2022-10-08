b5830557649db19856ec8e01e511b490a9252da5
smykad <dougsmyka@gmail.com>
2022-10-08 18:06:50 -0400

    h_o_6

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -5,14 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+
+using MovieList.Models;
 
 namespace MovieList.Controllers
 {
     public class HomeController : Controller
     {
-          public IActionResult Index()
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx)
         {
-            return View();
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            // retrieve from table and set to list
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View(movies);
         }
 
     }
