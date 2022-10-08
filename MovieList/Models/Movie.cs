a5c7ea2c0e9ad294af9b2ccc774bea3acd225dbd
smykad <dougsmyka@gmail.com>
2022-10-08 18:29:37 -0400

    Genre Commit

--- MovieList/Models/Movie.cs
+++ MovieList/Models/Movie.cs
@@ -19,7 +19,12 @@ namespace MovieList.Models
         [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
         public int? Rating { get; set; }
 
+        [Required(ErrorMessage = "Please enter a genre.")]
+        public string GenreId { get; set; }
+        public Genre Genre { get; set; }
         public string Slug =>
             Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
+
+
     }
 }
